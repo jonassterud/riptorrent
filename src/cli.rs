78498102a852c2082b45914e803ee7261bec17d3
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-09-14 15:24:01 +0000

    docs: some code documentation

--- src/cli.rs
+++ src/cli.rs
@@ -1,8 +1,9 @@
 use clap::Parser;
 
+/// CLI struct for Clap
 #[derive(Debug, Parser)]
 #[clap(author, version, about, long_about = None)]
 pub struct Cli {
-    ///// Path to torrent file
+    /// Path to torrent file
     pub path: String,
 }
