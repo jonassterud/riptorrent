7029b7d7f5908cbde8bb2422f73e60038ecbeadf
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-09-18 19:09:52 +0000

    feat: torrent struct

--- src/decode/mod.rs
+++ src/decode/mod.rs
@@ -1,15 +1,54 @@
 pub mod parse;
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use std::collections::BTreeMap;
 
 /// Represents the possible bencoded values
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Value {
     Integer(i64),
     ByteString(Vec<u8>),
     List(Vec<Value>),
     Dictionary(BTreeMap<Vec<u8>, Value>),
+}
+
+impl Value {
+    /// Check if two enum are of the same variant
+    pub fn variant_eq(&self, other: &Value) -> bool {
+        std::mem::discriminant(self) == std::mem::discriminant(other)
+    }
+
+    pub fn get_inner_integer(&self) -> Result<i64> {
+        if let Value::Integer(inner) = self {
+            Ok(inner.to_owned())
+        } else {
+            Err(anyhow!("Value is not a \"Integer\""))
+        }
+    }
+
+    pub fn get_inner_byte_string(&self) -> Result<Vec<u8>> {
+        if let Value::ByteString(inner) = self {
+            Ok(inner.to_owned())
+        } else {
+            Err(anyhow!("Value is not a \"ByteString\""))
+        }
+    }
+
+    pub fn get_inner_list(&self) -> Result<Vec<Value>> {
+        if let Value::List(inner) = self {
+            Ok(inner.to_owned())
+        } else {
+            Err(anyhow!("Value is not a \"List\""))
+        }
+    }
+
+    pub fn get_inner_dictionary(&self) -> Result<BTreeMap<Vec<u8>, Value>> {
+        if let Value::Dictionary(inner) = self {
+            Ok(inner.to_owned())
+        } else {
+            Err(anyhow!("Value is not a \"Dictionary\""))
+        }
+    }
 }
 
 /// Decodes bencoded data
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod cli;
 mod decode;
 mod tests;
+mod torrent;
+
+use std::convert::TryFrom;
 
 use cli::Cli;
-use decode::decode;
+use decode::{decode, Value};
+use torrent::Torrent;
 
 use anyhow::{anyhow, Result};
 use clap::Parser;
@@ -13,7 +17,9 @@ fn main() -> Result<()> {
 
     if let Ok(mut bytes) = std::fs::read(args.path) {
         let decoded = decode(&mut bytes, &mut 0).unwrap();
-        // ...
+        let torrent = Torrent::try_from(decoded.get(0).unwrap())?;
+
+        println!("{:?}", torrent);
     } else {
         return Err(anyhow!("Failed reading torrent file"));
     }
