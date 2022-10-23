1e44b81a85322644acf46ccc5d9a07e6f69e863a
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-10-23 14:03:50 +0000

    fix: stack overflow - replace `From` trait with `TryFrom`

--- crates/bcode/src/value/mod.rs
+++ crates/bcode/src/value/mod.rs
@@ -16,9 +16,14 @@ impl From<i64> for Value {
     }
 }
 
-impl From<Value> for i64 {
-    fn from(val: Value) -> Self {
-        val.into()
+impl TryFrom<Value> for i64 {
+    type Error = anyhow::Error;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Integer(out) => Ok(out),
+            _ => Err(anyhow!("Value is not a integer")),
+        }
     }
 }
 
@@ -28,9 +33,14 @@ impl From<Vec<u8>> for Value {
     }
 }
 
-impl From<Value> for Vec<u8> {
-    fn from(val: Value) -> Self {
-        val.into()
+impl TryFrom<Value> for Vec<u8> {
+    type Error = anyhow::Error;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::ByteString(out) => Ok(out),
+            _ => Err(anyhow!("Value is not a byte string")),
+        }
     }
 }
 
@@ -40,9 +50,14 @@ impl From<String> for Value {
     }
 }
 
-impl From<Value> for String {
-    fn from(val: Value) -> Self {
-        val.into()
+impl TryFrom<Value> for String {
+    type Error = anyhow::Error;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::ByteString(out) => Ok(String::from_utf8(out)?),
+            _ => Err(anyhow!("Value is not a byte string")),
+        }
     }
 }
 
@@ -52,9 +67,14 @@ impl From<Vec<Value>> for Value {
     }
 }
 
-impl From<Value> for Vec<Value> {
-    fn from(val: Value) -> Self {
-        val.into()
+impl TryFrom<Value> for Vec<Value> {
+    type Error = anyhow::Error;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::List(out) => Ok(out),
+            _ => Err(anyhow!("Value is not a list")),
+        }
     }
 }
 
@@ -64,8 +84,13 @@ impl From<BTreeMap<Vec<u8>, Value>> for Value {
     }
 }
 
-impl From<Value> for BTreeMap<Vec<u8>, Value> {
-    fn from(val: Value) -> Self {
-        val.into()
+impl TryFrom<Value> for BTreeMap<Vec<u8>, Value> {
+    type Error = anyhow::Error;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Dictionary(out) => Ok(out),
+            _ => Err(anyhow!("Value is not a dictionary")),
+        }
     }
 }
