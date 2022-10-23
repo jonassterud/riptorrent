577ab205e7bd2b2d1e54acdf89e729a5c0c21fac
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-10-23 14:06:20 +0000

    feat: `Torrent` from utf8

--- crates/torrent/src/torrent/from_utf8.rs
+++ crates/torrent/src/torrent/from_utf8.rs
@@ -16,30 +16,39 @@ fn get(map: &BTreeMap<Vec<u8>, bcode::Value>, key: &str) -> Result<bcode::Value>
 
 impl Torrent {
     pub fn from_utf8(vec: Vec<u8>) -> Result<Torrent> {
-        let main_map: BTreeMap<Vec<u8>, bcode::Value> = bcode::decode(&vec, &mut 0)?.into(); // ?
-        let info_map: BTreeMap<Vec<u8>, bcode::Value> = get(&main_map, "info")?.into();
+        let main_map: BTreeMap<Vec<u8>, bcode::Value> = bcode::decode(&vec, &mut 0)?.try_into()?; // ?
+        let info_map: BTreeMap<Vec<u8>, bcode::Value> = get(&main_map, "info")?.try_into()?;
 
-        let piece_length: i64 = get(&info_map, "piece length")?.into();
-        let pieces: Vec<u8> = get(&info_map, "piece length")?.into();
-        let private: Option<bool> = get(&info_map, "private").ok().map(|x| {
-            let temp: i64 = x.into();
-            temp != 0
-        });
+        let piece_length: i64 = get(&info_map, "piece length")?.try_into()?;
+        println!("1");
+        let pieces: Vec<u8> = get(&info_map, "pieces")?.try_into()?;
+        let private: Option<bool> = get(&info_map, "private")
+            .ok()
+            .map(|x| -> Result<bool> {
+                let temp: i64 = x.try_into()?;
+                Ok(temp != 0)
+            })
+            .transpose()?;
 
         let info: TorrentInfo = if info_map.contains_key(&b"files".to_vec()) {
-            let name: String = get(&info_map, "name")?.into();
+            let name: String = get(&info_map, "name")?.try_into()?;
 
-            let files_list: Vec<bcode::Value> = get(&main_map, "files")?.into();
+            let files_list: Vec<bcode::Value> = get(&info_map, "files")?.try_into()?;
             let files = files_list
                 .into_iter()
                 .map(|file_map| -> Result<File> {
-                    let file_map: BTreeMap<Vec<u8>, bcode::Value> = file_map.into();
+                    let file_map: BTreeMap<Vec<u8>, bcode::Value> = file_map.try_into()?;
 
-                    let length: i64 = get(&file_map, "length")?.into();
-                    let md5sum: Option<String> = get(&file_map, "md5sum").ok().map(|x| x.into());
-                    let path: Vec<bcode::Value> = get(&file_map, "path")?.into();
-                    let path: Vec<String> =
-                        path.into_iter().map(|x| -> String { x.into() }).collect();
+                    let length: i64 = get(&file_map, "length")?.try_into()?;
+                    let md5sum: Option<String> = get(&file_map, "md5sum")
+                        .ok()
+                        .map(|x| -> Result<String> { x.try_into() })
+                        .transpose()?;
+                    let path: Vec<bcode::Value> = get(&file_map, "path")?.try_into()?;
+                    let path: Vec<String> = path
+                        .into_iter()
+                        .map(|x| -> Result<String> { x.try_into() })
+                        .collect::<Result<Vec<String>>>()?;
 
                     Ok(File {
                         length,
@@ -57,9 +66,12 @@ impl Torrent {
                 files,
             })
         } else {
-            let name: String = get(&info_map, "name")?.into();
-            let length: i64 = get(&info_map, "length")?.into();
-            let md5sum: Option<String> = get(&info_map, "md5sum").ok().map(|x| x.into());
+            let name: String = get(&info_map, "name")?.try_into()?;
+            let length: i64 = get(&info_map, "length")?.try_into()?;
+            let md5sum: Option<String> = get(&info_map, "md5sum")
+                .ok()
+                .map(|x| -> Result<String> { x.try_into() })
+                .transpose()?;
 
             TorrentInfo::SingleFileInfo(SingleFileInfo {
                 piece_length,
@@ -71,27 +83,39 @@ impl Torrent {
             })
         };
 
-        let announce: String = get(&main_map, "announce")?.into();
-        let announce_list: Option<Vec<Vec<String>>> =
-            get(&main_map, "announce-list")
-                .ok()
-                .map(|val| -> Vec<Vec<String>> {
-                    let outer_list: Vec<bcode::Value> = val.into();
-                    outer_list
-                        .into_iter()
-                        .map(|x| -> Vec<String> {
-                            let inner_list: Vec<bcode::Value> = x.into();
-                            inner_list
-                                .into_iter()
-                                .map(|x| -> String { x.into() })
-                                .collect()
-                        })
-                        .collect()
-                });
-        let creation_date: Option<i64> = get(&main_map, "creation date").ok().map(|x| x.into());
-        let comment: Option<String> = get(&main_map, "comment").ok().map(|x| x.into());
-        let created_by: Option<String> = get(&main_map, "created by").ok().map(|x| x.into());
-        let encoding: Option<String> = get(&main_map, "encoding").ok().map(|x| x.into());
+        let announce: String = get(&main_map, "announce")?.try_into()?;
+        let announce_list: Option<Vec<Vec<String>>> = get(&main_map, "announce-list")
+            .ok()
+            .map(|val| -> Result<Vec<Vec<String>>> {
+                let outer_list: Vec<bcode::Value> = val.try_into()?;
+                outer_list
+                    .into_iter()
+                    .map(|x| -> Result<Vec<String>> {
+                        let inner_list: Vec<bcode::Value> = x.try_into()?;
+                        inner_list
+                            .into_iter()
+                            .map(|x| -> Result<String> { x.try_into() })
+                            .collect::<Result<Vec<String>>>()
+                    })
+                    .collect::<Result<Vec<Vec<String>>>>()
+            })
+            .transpose()?;
+        let creation_date: Option<i64> = get(&main_map, "creation date")
+            .ok()
+            .map(|x| -> Result<i64> { x.try_into() })
+            .transpose()?;
+        let comment: Option<String> = get(&main_map, "comment")
+            .ok()
+            .map(|x| -> Result<String> { x.try_into() })
+            .transpose()?;
+        let created_by: Option<String> = get(&main_map, "created by")
+            .ok()
+            .map(|x| -> Result<String> { x.try_into() })
+            .transpose()?;
+        let encoding: Option<String> = get(&main_map, "encoding")
+            .ok()
+            .map(|x| -> Result<String> { x.try_into() })
+            .transpose()?;
 
         Ok(Torrent {
             info,
--- crates/torrent/tests/decode_torrent.rs
+++ crates/torrent/tests/decode_torrent.rs
@@ -2,8 +2,9 @@ mod common;
 
 #[test]
 fn decode_torrent() {
-    let data =
-        std::fs::read("../../media/torrents/ubuntu-22.04.1-desktop-amd64.iso.torrent").unwrap();
-    // println!("{:?}", data);
-    println!("{:?}", torrent::Torrent::from_utf8(data).unwrap());
+    let path = std::env::current_dir().unwrap();
+    let path = path.join("/workspaces/riptorrent/crates/torrent/tests/common/ubuntu-22.04.1-desktop-amd64.iso.torrent");
+    let data = std::fs::read(path).unwrap();
+
+    torrent::Torrent::from_utf8(data).unwrap();
 }
