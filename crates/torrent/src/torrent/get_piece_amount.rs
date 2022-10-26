097f5cc92515b72412b258a865571619a99ffc31
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-10-26 18:51:06 +0000

    fix\!: get correct piece amount

--- crates/torrent/src/torrent/get_piece_amount.rs
+++ crates/torrent/src/torrent/get_piece_amount.rs
@@ -4,8 +4,8 @@ impl Torrent {
     /// Get amount of pieces in torrent.
     pub fn get_piece_amount(&self) -> usize {
         match &self.info {
-            TorrentInfo::SingleFileInfo(info) => info.pieces.len() % 20,
-            TorrentInfo::MultiFileInfo(info) => info.pieces.len() % 20,
+            TorrentInfo::SingleFileInfo(info) => info.pieces.len() / 20,
+            TorrentInfo::MultiFileInfo(info) => info.pieces.len() / 20,
         }
     }
 }
